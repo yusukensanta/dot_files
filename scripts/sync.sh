757b68f2cfdaac4fd6beeab6a095bd1e1a63da69
yusukensanta <yusuke.nakayama1218@gmail.com>
2023-08-24 18:16:17 +0900

    feat: add sync shell script

---
+++ scripts/sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TARGET_DIR=(nvim fish)
+
+for d in ${TARGET_DIR[@]};
+do
+  cp -r $HOME/.config/$d $PWD/.config/
+done
+
+cp /mnt/c/Users/yusuk/AppData/Roaming/alacritty/alacritty.yml $PWD/alacritty/alacritty.yml
