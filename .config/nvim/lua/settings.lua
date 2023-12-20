9433524867c3920271409382e9c8f42bae1bad80
yusukensanta <yusuke.nakayama1218@gmail.com>
2023-12-20 22:42:27 +0900

    feat: add format on save

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -12,8 +12,14 @@ require('mason-lspconfig').setup_handlers({ function(server)
   local opt = {
     on_attach = function(client, bufnr)
       vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { async = false }
+        end
+      })
 
-      local opts = { noremap=true, silent=true }
+      local opts = { noremap=true, silent=true, buffer=bufnr}
       vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
       vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
       vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
@@ -59,3 +65,5 @@ vim.api.nvim_set_keymap(
   ":tabnext<CR>",
   { noremap = true }
 )
+
+require('lspconfig').rust_analyzer.setup({})
