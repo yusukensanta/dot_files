ff3c313e8e8cfabab130da79b94f8d0dcd419e86
yusukensanta <yusuke.nakayama1218@gmail.com>
2024-01-22 18:05:28 +0900

    feat: update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,6 @@ source ~/.config/fish/path.fish
 source ~/.asdf/asdf.fish
 
 eval "$(zoxide init fish)"
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -6,3 +6,6 @@ set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git"'
 set -x ANDROID_HOME $HOME/work/android
 set -x PATH $PATH $ANDROID_HOME/cmdline-tools/bin
 set -x CHROME_EXECUTABLE /mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe
+
+set -x JIRA_API_TOKEN 'ATATT3xFfGF0NcbbOwQE3aJVLNAuWF6z2lrD6WQqZ3qQkv75m9gSzdchpPO_L9T1NJW5RA8RNzv9hnug_UhY366yHDHmLuahk24xSiY-Mr4JPLkqFm69F1JeDNxXTFn_UBMNyUhoCljCtRc5A1SeDIJLKBWuM2c6bbkONSuV1ZxfdRSCMqGi_-s=B342890B'
+set -x BROWSER "/mnt/c/Users/yusuk/AppData/Local/Programs/Opera GX/launcher.exe"
