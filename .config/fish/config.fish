823d39cf8e571a548e9fe37170983bc711789ae9
yusukensanta <yusuke.nakayama1218@gmail.com>
2024-05-26 21:24:47 +0900

    feat: update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,16 @@ if [ "(basename $PWD)" != "yusuken" ]
   cd ~
 end
 
+set system (cat /proc/version | grep "WSL")
+if [ -n "$system" ];
+  sudo rm -f /etc/resolv.conf
+  echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" | sudo tee /etc/resolv.conf > /dev/null
+end
+
 source ~/.config/fish/alias.fish
 source ~/.config/fish/path.fish
 source ~/.asdf/asdf.fish
+source ~/.cargo/env.fish
 
 eval "$(zoxide init fish)"
 
